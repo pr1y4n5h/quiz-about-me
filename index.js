8b5454f90024eae06165d698a8b729bf73a7f97d
Priyansh Maurya <priyansh.maurya@gmail.com>
2021-01-20 08:38:06 +0000

    commit final

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
 var readLineSync = require('readline-sync')
+const chalk = require('chalk');
 score = 0;
+
+console.log("****************************************************************************")
+console.log(chalk.greenBright.bold("\t\t\t\t\tWelcome to Priyansh's world! :)\t\t\t\t\t"));
+console.log(chalk.redBright.bold("This game is designed just to know, if my people know me or not!"));
+console.log("****************************************************************************")
+var name = readLineSync.question("Please enter your name first -- ")
+console.log(chalk.cyanBright.bold("Hi "+name.toUpperCase()+"! \nWe will ask you some questions "+name+" and will provide you 3 options. \nPlease type the correct option..."));
+var ready = readLineSync.question("So, are you ready "+name+"? Please reply with y/n  ")
+
 function play(ques,ans){
   var q = readLineSync.question(ques);
-  console.log(name+" you entered "+ q);
+  console.log(name.toUpperCase()+", you entered "+ q);
   if(q.toUpperCase()===ans.toUpperCase()){
-    console.log("Good! Correct one!");
+    console.log(chalk.green.bold("Good! Correct one!"));
     score = score + 10;
   }
   else{
-    console.log("Ooops! That was close!");
+    console.log(chalk.red.bold("Ooops! That was close!"));
     score = score - 10;
   }
   console.log("Your current score is ",score)
 }
-
-console.log("****************************************************************************")
-console.log("\t\t\t\t\tWelcome to Priyansh's world! :)\t\t\t\t\t")
-console.log("This game is designed just to know, if my people know me or not!")
-console.log("****************************************************************************")
-var name = readLineSync.question("Please enter your name first -- ")
-console.log("Hi "+name+"! \nWe will ask you some questions "+name+" and will provide you 3 options. \nPlease type the correct option...");
-var ready = readLineSync.question("So, are you ready "+name+"? Please reply with y/n  ")
 
 if(ready==="y"){
  console.log("Let's start the game then "+name+"! Try to guess a correct option\n");
@@ -36,9 +38,9 @@ console.log("*******************************************************************
  play("Ques 5 -> What is the IPL team that Priyansh follows? Your options are \n1> KKR \n2> MI \n2> CSK \nAnswer - ","CSK");
 console.log("****************************************************************************")
 
-console.log("End of quiz! Time for the result now!");
+console.log(chalk.yellow.bold("End of quiz! Time for the result now!"));
 console.log("Your final score is -->",score);
 }
 else{
-  console.log("Okay, no issues! But trust me, you'll start knowing me one day "+name+" ;) Goodbye for now!")
+  console.log("Okay, no issues! You'll know me one day "+name+" ;) Goodbye for now!")
 }
